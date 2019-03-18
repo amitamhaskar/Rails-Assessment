80d52fb72012afa795574e2c1abbdf91c1496e13
amita <amitamhaskar@gmail.com>
2019-03-18 07:02:37 +0530

    RotateKeyJobs class to handle status check of sidekiq jobs

--- app/controllers/data_encrypting_keys_controller.rb
+++ app/controllers/data_encrypting_keys_controller.rb
@@ -3,16 +3,15 @@ class DataEncryptingKeysController < ApplicationController
   before_action :load_encrypted_string, only: [:show, :destroy]
 
   def rotate
-    RotateKeysWorker.perform_async('rotate')
+    if RotateKeyJobs.add_new?
+      RotateKeysWorker.perform_async('rotate')
+    else
+      render json: { message: RotateKeyJobs.get_status_message },
+             status: :conflict
+    end
   end
 
   def status
-    if RotateKeyJobs.queued_jobs > 0 
-      render json: { status: DataEncryptingKey.ROTATE_STATUSes[:QUEUED] }
-    elsif RotateKeyJobs.busy_jobs > 0
-      render json: { status: DataEncryptingKey.ROTATE_STATUSes[:IN_PROGRESS] }
-    else
-      render json: { status: DataEncryptingKey.ROTATE_STATUSes[:EMPTY] }
-    end
+    render json: { status: RotateKeyJobs.get_status_message }    
   end
 end
--- app/models/rotate_key_jobs.rb
+++ app/models/rotate_key_jobs.rb
@@ -1,4 +1,10 @@
 class RotateKeyJobs
+  ROTATE_STATUS = {
+    EMPTY: "No key rotation queued or in progress",
+    QUEUED: "Key rotation has been queued",
+    IN_PROGRESS: "Key rotation is in progress"
+  }
+
   # Return the number of jobs that are enqueued
   # in the default sidekiq queue
   def self.queued_jobs
@@ -8,8 +14,26 @@ class RotateKeyJobs
   # Return the number of busy jobs in sidekiq
   def self.busy_jobs
     ps = Sidekiq::ProcessSet.new
+    busy_count = 0
     ps.each do |process|
-      return process['busy']   
+      busy_count += process['busy']   
+    end
+    busy_count
+  end
+
+  # Returns true if it is ok to add new job
+  def self.add_new?
+    self.queued_jobs == 0 && self.busy_jobs == 0
+  end
+
+  def self.get_status_message
+    message = if RotateKeyJobs.queued_jobs > 0 
+      ROTATE_STATUS[:QUEUED] 
+    elsif RotateKeyJobs.busy_jobs > 0
+      ROTATE_STATUS[:IN_PROGRESS] 
+    else
+      ROTATE_STATUS[:EMPTY]
     end
   end
+
 end
