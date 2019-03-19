855f103e453f3a62ebc46f8e7c9bf25e8eaa84aa
amita <amitamhaskar@gmail.com>
2019-03-19 20:34:33 +0530

    integration test

--- test/models/data_encrypting_key_test.rb
+++ test/models/data_encrypting_key_test.rb
@@ -27,4 +27,37 @@ class DataEncryptingKeyTest < ActiveSupport::TestCase
     # make sure the id of old and new primary keys is different
     assert_not_equal key.id, DataEncryptingKey.primary.id
   end
+
+  test "rotate_key" do
+    old_key_id = DataEncryptingKey.primary.id
+    # Create 2 encrypted strings
+    encrypted_string1 = EncryptedString.create!(value: "Test string for rotate 1")
+    encrypted_string2 = EncryptedString.create!(value: "Test string for rotate 2")
+    token1 = encrypted_string1.token
+    token2 = encrypted_string2.token
+
+    # Call the rotate function
+    DataEncryptingKey.rotate_key
+
+    # Make sure that the Data Encrypting key changed
+    new_key = DataEncryptingKey.primary
+    assert_not_equal old_key_id, new_key.id
+
+    # Get the string 1
+    encrypted_string = EncryptedString.find_by(token: token1)
+    # make sure the data encrypting key is the new one
+    assert_equal new_key.id, encrypted_string.data_encrypting_key.id
+    # make sure the original string can be retrieved.
+    assert_equal "Test string for rotate 1", encrypted_string.value
+
+    # Get the string 2
+    encrypted_string = EncryptedString.find_by(token: token2)
+    # make sure the data encrypting key is the new one
+    assert_equal new_key.id, encrypted_string.data_encrypting_key.id
+    # make sure the original string can be retrieved.
+    assert_equal "Test string for rotate 2", encrypted_string.value
+
+    # Make sure the old unused keys are deleted
+    assert DataEncryptingKey.find(old_key_id).nil?
+  end
 end
